04b833eb16063b6dc07b9060641117ad2321415d
AndrianNorth <disturbed7112@gmail.com>
2021-08-02 22:17:26 +0700

    HW07 init

--- app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/presenter/UsersPresenter.kt
+++ app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/presenter/UsersPresenter.kt
@@ -1,33 +1,36 @@
 package ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.presenter
 
 import com.github.terrakok.cicerone.Router
-import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.core.Scheduler
 import moxy.MvpPresenter
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.entity.GithubUser
-import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.repo.GithubUsersRepo
-import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.navigation.IScreens
+import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.navigation.IScreens
+import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.repo.IGithubUsersRepo
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.presenter.list.IUserListPresenter
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.view.UsersView
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.view.list.UserItemView
-import java.util.*
+import javax.inject.Inject
 
-class UsersPresenter(val usersRepo: GithubUsersRepo, val router: Router, val screens: IScreens) :
-    MvpPresenter<UsersView>() {
+class UsersPresenter(val uiScheduler: Scheduler) : MvpPresenter<UsersView>() {
+
+    @Inject lateinit var router: Router
+    @Inject lateinit var screens: IScreens
+    @Inject lateinit var usersRepo: IGithubUsersRepo
 
     class UsersListPresenter : IUserListPresenter {
         val users = mutableListOf<GithubUser>()
         override var itemClickListener: ((UserItemView) -> Unit)? = null
 
+        override fun getCount() = users.size
+
         override fun bindView(view: UserItemView) {
             val user = users[view.pos]
-            view.setLogin(user.login)
+            user.login?.let { view.setLogin(it) }
+            user.avatarUrl?.let {view.loadAvatar(it)}
         }
-        override fun getCount() = users.size
     }
 
     val usersListPresenter = UsersListPresenter()
-
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
@@ -35,25 +38,25 @@ class UsersPresenter(val usersRepo: GithubUsersRepo, val router: Router, val scr
         loadData()
 
         usersListPresenter.itemClickListener = { itemView ->
-            val userLogin = usersListPresenter.users[itemView.pos]
-            router.navigateTo(screens.singleUser(userLogin))
+            val user = usersListPresenter.users[itemView.pos]
+            router.navigateTo(screens.user(user))
         }
     }
 
     fun loadData() {
-        usersRepo.userObserver()
-            .subscribe(
-                {users ->
-                usersListPresenter.users.addAll(users)
+        usersRepo.getUsers()
+            .observeOn(uiScheduler)
+            .subscribe({ repos ->
+                usersListPresenter.users.clear()
+                usersListPresenter.users.addAll(repos)
                 viewState.updateList()
-            },{
-                it.printStackTrace()
+            }, {
+                println("Error: ${it.message}")
             })
     }
 
-    fun backClick(): Boolean {
+    fun backPressed(): Boolean {
         router.exit()
         return true
     }
 }
-
